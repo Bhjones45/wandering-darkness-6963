b8219656ed13e7902163a0ff69e3cefe75e7f0dd
Brett Jones <bhjones4545@gmail.com>
2021-08-06 09:39:55 -0600

    WIP

--- app/controllers/plots_controller.rb
+++ app/controllers/plots_controller.rb
@@ -2,4 +2,11 @@ class PlotsController < ApplicationController
   def index
     @plots = Plot.all
   end
+
+  def update
+    plot = Plot.find(params[:id])
+    plant = Plant.find_by_name(params[:name])
+    plant.delete(params[:plot_id])
+    redirect_to "/plots"
+  end
 end
